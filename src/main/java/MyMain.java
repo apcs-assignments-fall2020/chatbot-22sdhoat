fdf20ddf56de7cb2061dee99fb87927da0f352a6
22sdhoat <22sdhoat@athenian.org>
2020-09-01 00:09:53 +0000

    test bot glitch; 5 question chatbot using scanner

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,8 +3,23 @@ import java.util.Scanner;
 public class MyMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-
+        System.out.print("What is your name? ");
+		String name = scan.next();
+        System.out.println("Hello, " + name + "!");
+        System.out.print("What is your favorite color? ");
+        String color = scan.next();
+        System.out.println(color + ", is a pretty epic color!");
+        System.out.print("What is your favorite food? ");
+        String food = scan.next();
+        System.out.println(food + ", is delicious!");
+        System.out.print("How many siblings do you have? ");
+        String number = scan.next();
+        int siblings = Integer.parseInt(number);
+        System.out.println("Oh, you have " + siblings + "siblings, I have 1.");
+        System.out.print("What's your favorite number? ");
+        String favorite = scan.next();
+        int x = Integer.parseInt(favorite);
+        System.out.println("Though " + x + "is cool, 29 is a chad number.");
         scan.close();
     }
 }
